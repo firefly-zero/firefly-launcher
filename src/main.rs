1c839f77dba7d865735480e1ee51e56dad53ae47
gram <git@orsinium.dev>
2024-04-22 13:09:54 +0200

    launcher: show app names

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 #![no_main]
 extern crate alloc;
 use alloc::format;
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use core::cell::OnceCell;
+use firefly_meta::Meta;
 use firefly_rust::*;
 use talc::locking::AssumeUnlockable;
 use talc::{ClaimOnOom, Span, Talc, Talck};
@@ -26,7 +27,10 @@ pub const WIDTH: usize = 240;
 pub const HEIGHT: usize = 160;
 
 struct App {
-    path: String,
+    path:      String,
+    app_name:  String,
+    author_id: String,
+    app_id:    String,
 }
 
 #[no_mangle]
@@ -40,8 +44,18 @@ extern fn boot() {
         let author_path = format!("roms/{author_dir}");
         let app_dirs = sudo::DirBuf::list_dirs(&author_path);
         for app_dir in app_dirs.iter() {
-            let path = format!("{author_path}/{app_dir}");
-            apps.push(App { path });
+            let app_path = format!("{author_path}/{app_dir}");
+            let meta_path = format!("{app_path}/meta");
+            let meta_raw = sudo::load_file_buf(&meta_path);
+            let Ok(meta) = Meta::decode(meta_raw.data()) else {
+                continue;
+            };
+            apps.push(App {
+                path:      app_path,
+                app_name:  meta.app_name.to_string(),
+                author_id: meta.author_id.to_string(),
+                app_id:    meta.app_id.to_string(),
+            });
         }
     }
     unsafe { APPS.set(apps) }.ok().unwrap();
@@ -55,7 +69,7 @@ extern fn render() {
     let apps = unsafe { APPS.get() }.unwrap();
     for (i, app) in apps.iter().enumerate() {
         draw_text(
-            &app.path,
+            &app.app_name,
             &font,
             Point {
                 x: 10,
