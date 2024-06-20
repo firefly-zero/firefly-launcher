9cdc9dc35e88a60c0a12c0d2c3c79da522abe540
gram <git@orsinium.dev>
2024-06-20 18:33:19 +0200

    show basic app info

--- src/info_scene.rs
+++ src/info_scene.rs
@@ -1,6 +1,17 @@
 use crate::*;
 use firefly_rust::*;
 
+const LINE_HEIGHT: i32 = 12;
+
+static FIELDS: &[&str] = &[
+    "Author ID:",
+    "App ID:",
+    "Author name:",
+    "App name:",
+    "ROM size:",
+    "Data size:",
+];
+
 pub fn init(state: &mut State) {
     state.old_buttons = Buttons::default()
 }
@@ -10,7 +21,7 @@ pub fn update(state: &mut State) {
     let released_buttons = new_buttons.just_released(&state.old_buttons);
     if released_buttons.any() {
         state.transition_to(Scene::List);
-        open_menu();
+        // open_menu();
     }
     state.old_buttons = new_buttons;
 }
@@ -18,6 +29,18 @@ pub fn update(state: &mut State) {
 pub fn render(state: &State) {
     clear_screen(Color::White);
     let font = state.font.as_font();
-    let point = Point::new(62, HEIGHT / 2 - 12 / 2);
-    draw_text("TODO", &font, point, Color::DarkBlue);
+    for (text, i) in FIELDS.iter().zip(1..) {
+        let point = Point::new(6, LINE_HEIGHT * i);
+        draw_text(text, &font, point, Color::DarkBlue);
+    }
+    let app = &state.apps[state.pos];
+    render_info(&font, 1, &app.author_id);
+    render_info(&font, 2, &app.id);
+    render_info(&font, 3, &app.author_name);
+    render_info(&font, 4, &app.name);
+}
+
+fn render_info(font: &Font, i: i32, t: &str) {
+    let point = Point::new(100, LINE_HEIGHT * i);
+    draw_text(t, &font, point, Color::DarkBlue);
 }
