7ed86489367c1617ccf6d67c3088cf42db70e915
gram <git@orsinium.dev>
2025-07-27 19:44:49 +0200

    render scrollbar track

--- src/scroll.rs
+++ src/scroll.rs
@@ -7,11 +7,16 @@ const MARGIN_VERT: i32 = 3;
 const MARGIN_RIGHT: i32 = 4;
 const BAR_WIDTH: i32 = 6;
 const THUMB_HEIGHT: i32 = 10;
+
 /// The x coordinate of the bar's middle.
 const MIDDLE_X: i32 = WIDTH - BAR_WIDTH / 2 - MARGIN_RIGHT;
+const LEFT_X: i32 = MIDDLE_X - HALF_WIDTH;
+const RIGHT_X: i32 = MIDDLE_X + HALF_WIDTH;
 /// Half of the bar width.
 const HALF_WIDTH: i32 = BAR_WIDTH / 2;
 const ARROW_HEIGHT: i32 = HALF_WIDTH;
+const TRACK_MARGIN_VERT: i32 = MARGIN_VERT * 2 + ARROW_HEIGHT;
+const TRACK_HEIGHT: i32 = HEIGHT - TRACK_MARGIN_VERT * 2;
 
 pub struct ScrollBar {
     /// Index of the top item on the current page.
@@ -34,14 +39,16 @@ impl ScrollBar {
     pub fn render(&self) {
         self.draw_arrow_up();
         self.draw_arrow_down();
+        self.draw_track();
+        // self.draw_thumb();
     }
 
     fn draw_arrow_up(&self) {
         if self.top_pos == 0 {
             return;
         }
-        let left = Point::new(MIDDLE_X - HALF_WIDTH, MARGIN_VERT + ARROW_HEIGHT);
-        let right = Point::new(MIDDLE_X + HALF_WIDTH, MARGIN_VERT + ARROW_HEIGHT);
+        let left = Point::new(LEFT_X, MARGIN_VERT + ARROW_HEIGHT);
+        let right = Point::new(RIGHT_X, MARGIN_VERT + ARROW_HEIGHT);
         let top = Point::new(MIDDLE_X, MARGIN_VERT);
         draw_triangle(left, top, right, style());
     }
@@ -50,11 +57,32 @@ impl ScrollBar {
         if self.total - 1 <= self.top_pos + self.per_page {
             return;
         }
-        let left = Point::new(MIDDLE_X - HALF_WIDTH, HEIGHT - MARGIN_VERT - ARROW_HEIGHT);
-        let right = Point::new(MIDDLE_X + HALF_WIDTH, HEIGHT - MARGIN_VERT - ARROW_HEIGHT);
+        let left = Point::new(LEFT_X, HEIGHT - MARGIN_VERT - ARROW_HEIGHT);
+        let right = Point::new(RIGHT_X, HEIGHT - MARGIN_VERT - ARROW_HEIGHT);
         let bottom = Point::new(MIDDLE_X, HEIGHT - MARGIN_VERT);
         draw_triangle(left, bottom, right, style());
     }
+
+    fn draw_track(&self) {
+        if self.total <= self.per_page {
+            return;
+        }
+        let style = LineStyle {
+            color: Color::DarkBlue,
+            width: 1,
+        };
+        let a = Point::new(LEFT_X, TRACK_MARGIN_VERT);
+        let b = Point::new(RIGHT_X, TRACK_MARGIN_VERT);
+        draw_line(a, b, style);
+
+        let a = Point::new(LEFT_X, TRACK_HEIGHT + TRACK_MARGIN_VERT);
+        let b = Point::new(RIGHT_X, TRACK_HEIGHT + TRACK_MARGIN_VERT);
+        draw_line(a, b, style);
+    }
+
+    // fn draw_thumb(&self) {
+    //     // ...
+    // }
 }
 
 fn style() -> Style {
