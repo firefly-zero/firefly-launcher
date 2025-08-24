7541755a97bfff88e1c1881fd822e9ccea5995de
gram <git@orsinium.dev>
2025-08-24 09:21:04 +0200

    simplify

--- src/formatting.rs
+++ src/formatting.rs
@@ -1,38 +1,37 @@
 //! This module contains string formatting functions.
 //!
 //! You can achieve the same using [`alloc::format`].
-//! However, writing application-specific formatters,
-//! taking into account application-specific constraints,
-//! produces much smaller and faster code.
+//! However, writing application-specific formatters
+//! (and taking into account application-specific constraints)
+//! produces smaller and faster code.
 use alloc::string::String;
+use alloc::vec;
 
 /// Format the given date as YYYY-MM-DD.
 pub fn format_date(date: (u16, u8, u8)) -> String {
     let (y, m, d) = date;
-    let mut buf = String::new();
-    format_int04(&mut buf, y);
-    buf.push('-');
-    format_int02(&mut buf, m);
-    buf.push('-');
-    format_int02(&mut buf, d);
-    buf
+    let y = (y % 100) as u8;
+    let bytes = vec![
+        b'2',
+        b'0',
+        digit2(y),
+        digit1(y),
+        b'-',
+        digit2(m),
+        digit1(m),
+        b'-',
+        digit2(d),
+        digit1(d),
+    ];
+    unsafe { String::from_utf8_unchecked(bytes) }
 }
 
-/// Format a 4-digit integer padding it with zeros on the left.
-pub fn format_int04(buf: &mut String, val: u16) {
-    format_digit(buf, (val / 1000) as u8);
-    format_digit(buf, ((val / 100) % 10) as u8);
-    format_digit(buf, ((val / 10) % 10) as u8);
-    format_digit(buf, (val % 10) as u8);
+/// Convert to a byte character the second digit of an integer.
+const fn digit2(dig: u8) -> u8 {
+    digit1(dig / 10)
 }
 
-/// Format a 2-digit integer padding it with zeros on the left.
-pub fn format_int02(buf: &mut String, val: u8) {
-    format_digit(buf, val / 10);
-    format_digit(buf, val % 10);
-}
-
-/// Format a single-digit integer.
-pub fn format_digit(buf: &mut String, dig: u8) {
-    buf.push((b'0' + dig) as char);
+/// Convert to a byte character the first digit of an integer.
+const fn digit1(dig: u8) -> u8 {
+    b'0' + (dig % 10)
 }
